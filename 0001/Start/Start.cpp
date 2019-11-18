a9853c519d0ee49f1e22ef0568aa934a0d1bf5d2
EPJcoder <jogosdigitaissp@gmail.com>
2019-11-18 19:10:26 -0300

    Primeiro Programa.

---
+++ 0001/Start/Start.cpp
@@ -0,0 +1,15 @@
+/*
+	Programador: Euclydes Penteado Junior
+	Data: 18/11/2019
+	e-mail: jogosdigitaissp@gmail.com
+	Objetivo: Imprimir uma string de caracter
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "START";
+
+	return 0;
+}
