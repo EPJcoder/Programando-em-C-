8abce85e1679221d2145e6047f3edaa2ac50aeec
EPJcoder <jogosdigitaissp@gmail.com>
2019-11-19 17:24:07 -0300

    Imprimir uma string de caracteres

--- 0001/Start/Start.cpp
+++ 0001/Start/Start.cpp
@@ -2,7 +2,7 @@
 	Programador: Euclydes Penteado Junior
 	Data: 18/11/2019
 	e-mail: jogosdigitaissp@gmail.com
-	Objetivo: Imprimir uma string de caracteres
+	Objetivo: Imprimir uma string de caracteres: START
 */
 
 #include <iostream>
