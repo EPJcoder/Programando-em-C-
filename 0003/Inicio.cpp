a05fd67bde0b25145cd89d051c765c6014cf832d
EPJcoder <jogosdigitaissp@gmail.com>
2019-11-22 11:08:08 -0300

    Imprimir múltiplas linhas de string com apenas uma instrução

---
+++ 0003/Inicio.cpp
@@ -0,0 +1,15 @@
+/*
+	Programador: Euclydes Penteado Junior
+	Data: 22/11/2019
+	e-mail: jogosdigitaissp@gmail.com
+	Objetivo: Imprimir múltiplas linhas de string com uma única instrução
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Programando\nem\nC\nplus\nplus\n";
+
+	return 0;
+}
