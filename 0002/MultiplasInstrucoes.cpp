fe573c109051c2c3cb5fc93b9d6a049cc3ddf520
EPJcoder <jogosdigitaissp@gmail.com>
2019-11-24 06:12:19 -0300

    Múltiplas instruções imprimindo uma string de caracteres

---
+++ 0002/MultiplasInstrucoes.cpp
@@ -0,0 +1,19 @@
+/*
+	Programador: Euclydes Penteado Junior
+	Data: 18/11/2019
+	e-mail: jogosdigitaissp@gmail.com
+	Objetivo: Múltiplas instruções imprimindo uma string de caracteres
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	std::cout << "Multiplas ";
+	std::cout << "instrucoes imprimindo ";
+	std::cout << "uma string de caracteres em linha unica" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
