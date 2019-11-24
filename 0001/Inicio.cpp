d47c622f85577da030ad6a487e35a64d7ff1c873
EPJcoder <jogosdigitaissp@gmail.com>
2019-11-24 05:51:28 -0300

    Imprimir uma string de caracteres

--- 0001/Inicio.cpp
+++ 0001/Inicio.cpp
@@ -2,14 +2,16 @@
 	Programador: Euclydes Penteado Junior
 	Data: 18/11/2019
 	e-mail: jogosdigitaissp@gmail.com
-	Objetivo: Imprimir uma string de caracteres: START
+	Objetivo: Imprimir uma string de caracteres
 */
 
 #include <iostream>
+#include <cstdlib>
 
 int main()
 {
-	std::cout << "START";
+	std::cout << "Iniciando a jornada!" << std::endl;
 
+	system("PAUSE");
 	return 0;
 }
