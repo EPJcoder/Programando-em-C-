491f34a17c39032e73935e43165b7acdedac1cf4
EPJcoder <jogosdigitaissp@gmail.com>
2019-11-27 16:27:26 -0300

    Definir classe separada da função principal

---
+++ 0010/CadernoDeNotas.h
@@ -0,0 +1,40 @@
+/*
+	Programador: Euclydes Penteado Junior
+	Data: 27/11/2019
+	e-mail: jogosdigitaissp@gmail.com
+	Objetivo: Definir a classe CadernoDeNotas
+	independente da função main. Ou seja, em
+	um arquivo separado.
+*/
+
+#include <iostream>
+#include <string>
+
+class CadernoDeNotas
+{
+public:
+	explicit CadernoDeNotas( std::string nome )
+		:	cursoNome( nome )
+	{
+
+	}
+
+	void setCursoNome( std::string nome )
+	{
+		cursoNome = nome;
+	}
+
+	std::string getCursoNome() const
+	{
+		return cursoNome;
+	}
+
+	void ExibirMensagem() const
+	{
+		std::cout << "Bem-Vindo ao Caderno de Notas: " << getCursoNome()
+			<< std::endl;
+	}
+
+private:
+	std::string cursoNome;
+};
